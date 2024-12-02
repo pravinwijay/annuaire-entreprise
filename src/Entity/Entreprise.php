25bfe4cc40ba28506902e6bfd603ca36181b6726
Pravin Wijay <wijayasuntharam.pravin@gmail.com>
2024-12-02 17:39:06 +0100

    Controller

--- src/Entity/Entreprise.php
+++ src/Entity/Entreprise.php
@@ -6,6 +6,7 @@ use ApiPlatform\Metadata\ApiResource;
 use App\Repository\EntrepriseRepository;
 use Doctrine\ORM\Mapping as ORM;
 
+
 #[ORM\Entity(repositoryClass: EntrepriseRepository::class)]
 #[ApiResource]
 class Entreprise
