cbafc5dde4f0b04d161c0979131e5a14ac5861e3
ayoubenn03 <ayoub.ennouary03@gmail.com>
2024-12-03 12:44:47 +0100

    Ajoute de systeme de routes

---
+++ src/components/Entreprise.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue';
+
+const entreprises = ref([
+  { id: 1, nom: 'Entreprise 1', secteur: 'IT' },
+  { id: 2, nom: 'Entreprise 2', secteur: 'Finance' },
+]);
+</script>
+
+<template>
+  <div>
+    <h1>Liste des entreprises</h1>
+    <ul>
+      <li v-for="entreprise in entreprises" :key="entreprise.id">
+        {{ entreprise.nom }} - Secteur : {{ entreprise.secteur }}
+      </li>
+    </ul>
+  </div>
+</template>
---
+++ src/components/NotFound.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+      <h1>404 - Page non trouvée</h1>
+      <p>La page que vous recherchez n'existe pas.</p>
+    </div>
+  </template>
+  
